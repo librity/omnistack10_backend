9d2b19ea253f8e4f5d91620a161306d2f62929a9
Luis Geniole <luis.geniole@gmail.com>
2020-01-17 22:32:48 -0300

    finish Search

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/parseStringAsArray");
 
 module.exports = {
   async index(request, response) {
@@ -23,7 +24,7 @@ module.exports = {
 
     const { name = login, avatar_url, bio } = apiResponse.data;
 
-    const techsArray = techs.split(",").map(tech => tech.trim());
+    const techsArray = parseStringAsArray(techs);
 
     const location = {
       type: "Point",
@@ -40,5 +41,13 @@ module.exports = {
     });
 
     return response.json(dev);
-  }
+  },
+
+  /**
+   * TO DO
+   */
+
+  async update() {},
+
+  async destroy() {}
 };
