a7126e414d24899ff2fe8399097d70858b990144
Luis Geniole <luis.geniole@gmail.com>
2020-01-19 01:04:06 -0300

    Add Socket.io

---
+++ src/utils/calculateDistance.js
@@ -0,0 +1,27 @@
+function degreesToRadians(deggrees) {
+  return deggrees * (Math.PI / 180);
+}
+
+module.exports = function getDistanceFromLatLonInKm(
+  centerCoordinates,
+  pointCoordinates
+) {
+  const radius = 6371;
+
+  const { latitude: lat1, longitude: lon1 } = centerCoordinates;
+  const { latitude: lat2, longitude: lon2 } = pointCoordinates;
+
+  const dLat = degreesToRadians(lat2 - lat1);
+  const dLon = degreesToRadians(lon2 - lon1);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(degreesToRadians(lat1)) *
+      Math.cos(degreesToRadians(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const center = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = radius * center;
+
+  return distance;
+};
